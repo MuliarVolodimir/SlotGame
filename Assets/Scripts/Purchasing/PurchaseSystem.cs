32f998a6613be854608fbe50864d35533afcea69
destrouo <destrouo@gmail.com>
2024-06-15 21:38:00 +0300

    no iap for test

--- Assets/Scripts/Purchasing/PurchaseSystem.cs
+++ Assets/Scripts/Purchasing/PurchaseSystem.cs
@@ -1,37 +1,37 @@
 using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Purchasing;
+//using UnityEngine.Purchasing;
 
 public class PurchaseSystem : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _coins;
     [SerializeField] PopupScreen _popupScreen;
 
-    public void OnPurchaseComplite(Product product)
-    {
-        switch (product.definition.id)
-        {
-            case "spaceSlot10000Coins":
-                PurchaseCoins();
-                break;
-            default:
-                break;
-        }
-    }
+    // public void OnPurchaseComplite(Product product)
+    // {
+    //     switch (product.definition.id)
+    //     {
+    //         case "spaceSlot10000Coins":
+    //             PurchaseCoins();
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // }
 
-    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
-    {
-        switch (product.definition.id)
-        {
-            case "spaceSlot10000Coins":
-                _popupScreen.ShowMessage($"FAILD TO PURCHASE COINS! \n {failureReason}");
-                break;
-            default:
-                break;
-        }
+    // public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    // {
+    //     switch (product.definition.id)
+    //     {
+    //         case "spaceSlot10000Coins":
+    //             _popupScreen.ShowMessage($"FAILD TO PURCHASE COINS! \n {failureReason}");
+    //             break;
+    //         default:
+    //             break;
+    //     }
         
-    }
+    // }
 
     private void PurchaseCoins()
     {
